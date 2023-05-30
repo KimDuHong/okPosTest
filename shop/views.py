ccf0b676ec1931bfc06b5e0df1b9d65dfed0f288
KimDuHong <art970@naver.com>
2023-05-30 23:04:23 +0900

    add GET / POST / PATCH

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
-from django.db import transaction
-from django.db import IntegrityError
+from django.db import transaction, IntegrityError
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
@@ -10,6 +11,7 @@ from .models import Product, Tag, ProductOption
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.prefetch_related("tag_set", "option_set")
+
     serializer_class = ProductSerializer
 
     @transaction.atomic
@@ -24,32 +26,128 @@ class ProductViewSet(ModelViewSet):
 
         product = serializer.instance
 
-        # Create ProductOptions
+        product_options = []
         for option in option_data:
-            ProductOption.objects.create(product=product, **option)
+            if "name" in option and "price" in option:
+                try:
+                    price = int(option["price"])
+                except ValueError:
+                    raise ValidationError("가격은 숫자로 입력해야 합니다.")
 
-        try:
-            for tag in tag_data:
-                if "name" not in tag:
-                    raise ValidationError("태그명은 필수 입력 값입니다.")
-
-                if "pk" in tag:
-                    tag_obj = Tag.objects.get(pk=tag["pk"], name=tag["name"])
-                else:
-                    tag_obj = Tag.objects.create(name=tag["name"])
-                product.tag_set.add(tag_obj)
-
-        except Tag.DoesNotExist:
-            raise NotFound
-
-        except IntegrityError:
-            already_tag = tag.get("name", None)
-            if already_tag:
-                raise ValidationError(f"해당 태그명 ({already_tag}) 은/는  이미 존재합니다.")
+                product_options.append(ProductOption(product=product, **option))
             else:
+                raise ValidationError("옵션명과 가격은 필수 입력 값입니다.")
+
+        ProductOption.objects.bulk_create(product_options)
+
+        new_tags = []
+        exist_tags = []
+        for tag in tag_data:
+            if "name" in tag and "pk" not in tag:
+                new_tags.append(Tag(name=tag["name"]))
+            elif "name" in tag and "pk" in tag:
+                exist_tags.append(tag)
+            elif "name" not in tag:
                 raise ValidationError("태그명은 필수 입력 값입니다.")
 
-        headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        if new_tags:
+            try:
+                Tag.objects.bulk_create(new_tags)
+            except IntegrityError:
+                raise ValidationError("태그명은 중복될 수 없습니다.")
+
+        all_tags = Tag.objects.filter(
+            Q(name__in=[tag.name for tag in new_tags])
+            | Q(
+                pk__in=[tag["pk"] for tag in exist_tags],
+                name__in=[tag["name"] for tag in exist_tags],
+            )
         )
+
+        product.tag_set.set(all_tags)
+
+        return Response(
+            ProductSerializer(product).data,
+            status=status.HTTP_201_CREATED,
+        )
+
+    @transaction.atomic
+    def update(self, request, pk, *args, **kwargs):
+        product = get_object_or_404(Product, pk=pk)
+        data = request.data
+
+        option_data = data.pop("option_set", [])
+        tag_data = data.pop("tag_set", [])
+
+        existing_option_pks = [
+            option.get("pk") for option in option_data if option.get("pk")
+        ]
+        ProductOption.objects.filter(
+            product=product,
+        ).exclude(
+            pk__in=existing_option_pks,
+        ).delete()
+
+        new_option_objects = []
+        exist_option_objects = []
+        for option in option_data:
+            if "name" not in option:
+                raise ValidationError("옵션명은 필수 입력 값입니다.")
+
+            if "price" not in option:
+                raise ValidationError("가격은 필수 입력 값입니다.")
+            else:
+                try:
+                    price = int(option["price"])
+                except ValueError:
+                    raise ValidationError("가격은 숫자로 입력해야 합니다.")
+
+            if "pk" in option:
+                exist_option_objects.append(
+                    ProductOption(
+                        pk=option["pk"],
+                        product=product,
+                        name=option["name"],
+                        price=option["price"],
+                    )
+                )
+            else:
+                new_option_objects.append(
+                    ProductOption(
+                        product=product, name=option["name"], price=option["price"]
+                    )
+                )
+
+        ProductOption.objects.bulk_update(
+            exist_option_objects,
+            fields=["name", "price"],
+        )
+        ProductOption.objects.bulk_create(new_option_objects)
+
+        new_tags = []
+        exist_tags = []
+        for tag in tag_data:
+            if "name" in tag and "pk" not in tag:
+                new_tags.append(Tag(name=tag["name"]))
+            elif "name" in tag and "pk" in tag:
+                exist_tags.append(tag)
+            elif "name" not in tag:
+                raise ValidationError("태그명은 필수 입력 값입니다.")
+
+        if new_tags:
+            try:
+                Tag.objects.bulk_create(new_tags, ignore_conflicts=True)
+            except IntegrityError:
+                raise ValidationError("태그명은 중복될 수 없습니다.")
+
+        all_tags = Tag.objects.filter(
+            Q(name__in=[tag.name for tag in new_tags])
+            | Q(
+                pk__in=[tag["pk"] for tag in exist_tags],
+                name__in=[tag["name"] for tag in exist_tags],
+            )
+            | Q(product=product)
+        )
+        product.tag_set.set(all_tags)
+
+        return Response(ProductSerializer(product).data)
